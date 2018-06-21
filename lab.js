5c71825cb4e3076dc8c890a965dc310848c5ccd3
jakerochford <tufts.rochford@gmail.colm>
2018-06-20 21:43:01 -0400

    Finished messages part 2

--- lab.js
+++ lab.js
@@ -1,1 +1,17 @@
-// Your JavaScript goes here...
+function parse() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+	  var parsed = JSON.parse(this.responseText);
+      i = 0;
+      x = " ";
+      for (i in parsed) {
+        x += parsed[i].content + " " + parsed[i].username + "<br>";
+      }
+
+      document.getElementById("messages").innerHTML = x;
+    }
+  };
+  xmlhttp.open("GET", "data.json", true);
+  xmlhttp.send();
+}
